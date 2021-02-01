a08a8d33689fbc181297e7be76e45dfc8b5bdd67
erastus <erastuskariuki15@gmail.com>
2021-02-01 09:57:55 +0300

    deployment to heroku

--- mtaafix/settings.py
+++ mtaafix/settings.py
@@ -14,7 +14,7 @@ import os
 import django_heroku
 import dj_database_url
 
-from decouple import config,Csv
+from decouple import config, Csv
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
