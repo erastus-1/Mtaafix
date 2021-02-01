47f645b6eccc43f112d7f5af3df1b865ad3e6b68
erastus <erastuskariuki15@gmail.com>
2021-02-01 09:02:29 +0300

    altered urls

--- mtaafix/urls.py
+++ mtaafix/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('',include('mtaapp.urls')),
+    url('accounts/', include('registration.backends.simple.urls')),
+    url('accounts/logout/', views.logout_then_login, {"next_page": '/'}),
+
 ]
